ddcd5ff0c8f4334776cc5f10294551691869ee6c
semenov ramazan <rombik.s@inbox.ru>
2022-04-28 14:40:55 +0300

    Добавьте файлы проекта.

---
+++ ConAppSsis/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.SqlServer.Dts.Runtime;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppSsis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pkgLocation;
+            Package pkg;
+            Application app;
+            DTSExecResult pkgResults;
+
+            pkgLocation =
+              @"C:\Users\lenovo\source\repos\Integration Services Project1\Integration Services Project1\Package.dtsx";
+            app = new Application();
+            pkg = app.LoadPackage(pkgLocation, null);
+            pkgResults = pkg.Execute();
+
+            Console.WriteLine(pkgResults.ToString());
+            Console.ReadKey();
+        }
+    }
+}
