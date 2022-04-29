1ce6efaa38b41c7bd450fdecec4d1d6652745ee2
semenov ramazan <rombik.s@inbox.ru>
2022-04-29 11:53:57 +0300

    cv

--- ConAppSsis/Program.cs
+++ ConAppSsis/Program.cs
@@ -7,6 +7,18 @@ using System.Threading.Tasks;
 
 namespace ConAppSsis
 {
+    //dtexec.exe /F "C:\Users\lenovo\source\repos\Integration Services Project1\Integration Services Project1\Package.dtsx"
+    class MyEventListener : DefaultEvents
+    {
+       
+        public override bool OnError(DtsObject source, int errorCode, string subComponent,
+          string description, string helpFile, int helpContext, string idofInterfaceWithError)
+        {
+            // Add application-specific diagnostics here.  
+            Console.WriteLine("Error in {0}/{1} : {2}", source, subComponent, description);
+            return false;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -15,11 +27,11 @@ namespace ConAppSsis
             Package pkg;
             Application app;
             DTSExecResult pkgResults;
-
+            MyEventListener eventListener = new MyEventListener();
             pkgLocation =
               @"C:\Users\lenovo\source\repos\Integration Services Project1\Integration Services Project1\Package.dtsx";
             app = new Application();
-            pkg = app.LoadPackage(pkgLocation, null);
+            pkg = app.LoadPackage(pkgLocation, eventListener);
             pkgResults = pkg.Execute();
 
             Console.WriteLine(pkgResults.ToString());
